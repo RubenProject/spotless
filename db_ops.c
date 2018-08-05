5105eb30a26d48e6cf1b632a3143b9d7af083977
rubenproject <r.vdwaal1995@gmail.com>
2018-08-06 00:17:03 +0200

    progress

--- db_ops.c
+++ db_ops.c
@@ -7,6 +7,16 @@
 #include "sys/types.h"
 
 #define MEDIA_FOLDER "media";
+#define STMT_MAX 500;
+
+
+typedef struct cv_pairs {
+    char table[30];
+    char column[10][100];
+    char value[10][100];
+};
+
+
 
 
 void *db_open2(const char *db_path) {
@@ -28,6 +38,48 @@ void db_close2(void **db_handle) {
 }
 
 
+void db_get_by_key(void **db, const char *key){
+} 
+
+
+void db_get_by_tag(void **db, const char *tag){
+}
+
+
+void db_add(void **db, const char *table_name, FILE *dp){
+    char stmt[STMT_MAX] = "INSERT INTO \0";
+    strcat(stmt, table_name);
+    strcat(stmt, " (" 
+    strcat(column_names);
+    strcat(stmt, ") VALUES ("); 
+    strcat(stmt, video_title); 
+    strcat(stmt, ", 'test description');");
+    fprintf(stdout, "%s\n", stmt);
+    sqlite3_exec(db, stmt, 0, 0, 0);
+}
+
+
+void parse_file(FILE *fp, struct cv_pair **cv){
+    //video title
+    char t[100];
+    strcpy(t, fp->d_name);
+    int i;
+    for (i = strlen(t - 1); i >= 0; i--){
+        if (t[i] == '.'){
+            t[i] = '\0';
+            break;
+        }
+        t[i] = '\0';
+    }
+    strcpy(cv.column[0], "video_title");
+    strcpy(cv.value[0], t);
+    //description
+}
+
+
+
+
+
 void db_update(void **db_handle){
     //loop through media folder
     struct dirent *dp;
@@ -39,16 +91,10 @@ void db_update(void **db_handle){
         exit(0);
     }
     //each file put entry in database
-    struct stat stbuff;
-    char stmt[500];
-    strcpy(stmt, "INSERT INTO media_1 (video_title, description, date_added) VALUES ('test title', 'test description');");
     while ((dp = readdir(dfd)) != NULL){
         if (dp->d_name[0] != '.'){
             fprintf(stdout, "%s\n", dp->d_name);
-            sqlite3_exec(db_handle, stmt, 0, 0, 0);
-
+            db_add(db, "media_1", dp);
         }
     }
-
-    
 }
